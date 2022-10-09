2ea276a33dcb53bb9a6799359e3299bcf310573a
Jorge Luis <sejidjorge@gmail.com>
2022-10-09 20:56:53 -0300

    ajustes de configuracao

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -6,7 +6,7 @@ const GlobalStyle = createGlobalStyle`
         margin: 0;
         box-sizing: border-box;
     };
-    html, body {
+html, body {
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
@@ -14,6 +14,6 @@ const GlobalStyle = createGlobalStyle`
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
-    `;
+`;
 
 export default GlobalStyle;
