04b0ef183446769a5638fc0b294329cb0524148a
Jorge Luis <sejidjorge@gmail.com>
2022-10-09 21:02:07 -0300

    ajustes finais de config

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,11 @@
-
 function App() {
-  return (
-    <div className="App">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+        </div>
+    );
 }
 
 export default App;
